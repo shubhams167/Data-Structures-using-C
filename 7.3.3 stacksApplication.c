f9bbfac1b8a1d7fafa8a232790be7726f5705b5d
Shubham Singh <shubhams.167@gmail.com>
2019-03-04 06:34:13 +0530

    Updated 7.3.3

--- 7.3.3 stacksApplication.c
+++ 7.3.3 stacksApplication.c
@@ -2,7 +2,7 @@
 
 #include<stdio.h>
 #include<string.h>			//	For strlen() and strcat()
-#include<ctype.h>			//	For isalpha()
+#include<ctype.h>			//	For isalnum()
 #define MAX 50
 
 char stack[MAX];
@@ -11,67 +11,61 @@ int top = -1;
 //	Function to push into the stack
 int PUSH(char val)
 {
-	if(top == MAX - 1)
-		printf("Error. Stack OverFlow.\n");
-	else
-		stack[++top] = val;
+	stack[++top] = val;
 }
 
 //	Function to pop from the stack
 char POP()
 {
-	if(top == -1)
-		printf("Error. Stack UnderFlow!\n");
-	else
-		return stack[top--];
+	return stack[top--];
 }
 
 //	Function to get priority of an operator
 int getPriority(char op)
 {
-	if(op == '/' || op == '*' || op == '%')
-		return 1;
-	else
+	if(op == '(')
 		return 0;
+	if(op == '+' || op == '-')
+		return 1;
+	if(op == '/' || op == '*')
+		return 2;
 }
 
 //	Function to convert infix into postfix
 char *infixToPostfix(char infix[])
 {
-	static char postfix[40];				//	Declare postfix as static otherwise after returning, postfix will get destroyed!
+	static char postfix[40];//	Declare postfix as static otherwise after returning, postfix will get destroyed!
 	int j = 0;
-	PUSH('(');								//	To push '(' into the stack
 	for(int i = 0; i < strlen(infix); i++)
 	{
-		if(isalpha(infix[i]))
+		if(isalnum(infix[i]))
 			postfix[j++] = infix[i];
 		else
-			if(infix[i] == ')' )
-			{	
-				while(stack[top] != '(')
-					postfix[j++] = POP();
-				POP();
-			}
-			else
+		if(infix[i] == '(')
+			PUSH(infix[i]);
+		else
+		if(infix[i] == ')' )
+		{	
+			while(stack[top] != '(')
+				postfix[j++] = POP();
+			POP();
+		}
+		else
+		{
+			while(getPriority(infix[i]) <= getPriority(stack[top]))
 			{
-				if(getPriority(infix[i]) < getPriority(stack[top]))
-				{
-					postfix[j++] = POP();
-				}
-				PUSH(infix[i]);
+				postfix[j++] = POP();
 			}
+			PUSH(infix[i]);
+		}
 	}
+	while(top != -1)
+		postfix[j++] = POP();
 	return postfix;
 }
 
 int main()
 {
-	char infix[40];
-	printf("Enter Infix expression: ");
-	scanf("%s",infix);
-	
-	strcat(infix,")");							//	To add ')' at the end of the expression
-	
-	printf("\nPostfix Expression: %s",infixToPostfix(infix));
+	cout<<"Final Result: "<<infixToPostfix("(5+3)*4");
 	return 0;
 }
